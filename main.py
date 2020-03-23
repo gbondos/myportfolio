7a67511b0bb927eded82c392522fb9b099240fc4
Moses Gbondo <gbondos@berea.edu>
2020-03-23 17:28:02 -0400

    changes

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
